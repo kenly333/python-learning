05cc4ae1b8db1256546915ad07638bae236636c2
likuan <likuan33@126.com>
2018-11-07 15:22:31 +0800

    mqtt

---
+++ mqtt_client/paho_mqtt.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+
+# 连接成功回调函数
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    # 连接完成之后订阅gpio主题
+    client.subscribe("devcloud/code/hub")
+
+
+# 消息推送回调函数
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    try:
+        # 请根据实际情况改变MQTT代理服务器的IP地址
+        client.connect("172.16.210.150", 1883, 60)
+        client.loop_forever()
+    except KeyboardInterrupt:
+        client.disconnect()
